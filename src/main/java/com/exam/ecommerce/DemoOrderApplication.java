ac1cef7e14f04cb2cbff5e45fce3e7b56fb8b30c
vishnuKarnati123 <kvishnuvardhan01@gmail.com>
2022-03-17 17:14:16 +0530

    version 2

--- src/main/java/com/exam/ecommerce/DemoOrderApplication.java
+++ src/main/java/com/exam/ecommerce/DemoOrderApplication.java
@@ -1,11 +1,19 @@
 package com.exam.ecommerce;
 
-import org.modelmapper.ModelMapper;import org.springframework.boot.SpringApplication;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import com.exam.ecommerce.feignclient.FeignClientUtil;
+
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients(clients= {FeignClientUtil.class})
 public class DemoOrderApplication {
 
 	@Bean
@@ -14,11 +22,12 @@ public class DemoOrderApplication {
 		return new ModelMapper();
 	}
 	
-	@Bean
-	public RestTemplate getRestTemplate()
-	{
-		return new RestTemplate();
-	}
+	/*
+	 * @Bean
+	 * 
+	 * @LoadBalanced public RestTemplate getRestTemplate() { return new
+	 * RestTemplate(); }
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoOrderApplication.class, args);
 	
