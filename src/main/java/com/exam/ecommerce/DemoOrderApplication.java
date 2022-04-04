e897d6cfa9a4248278879f723c78facb00088abf
vishnuKarnati123 <101550891+vishnuKarnati123@users.noreply.github.com>
2022-04-04 11:16:53 +0530

    version 3

--- src/main/java/com/exam/ecommerce/DemoOrderApplication.java
+++ src/main/java/com/exam/ecommerce/DemoOrderApplication.java
@@ -3,25 +3,28 @@ package com.exam.ecommerce;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 import com.exam.ecommerce.feignclient.FeignClientUtil;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
+
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients(clients= {FeignClientUtil.class})
+@EnableFeignClients(clients = { FeignClientUtil.class })
+@OpenAPIDefinition(info =@Info(title="Order API", description = "Order MicroService") )
 public class DemoOrderApplication {
 
 	@Bean
-	public ModelMapper modelMapper()
-	{
+	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-	
+
 	/*
 	 * @Bean
 	 * 
@@ -30,7 +33,10 @@ public class DemoOrderApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoOrderApplication.class, args);
-	
+
 	}
+
+	
+	 
 
 }
